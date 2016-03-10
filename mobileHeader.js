f38abcf5e96ab3c507b8d95587293ee1c3ad3999
2pix <topix@mail.de>
2016-03-10 08:05:23 +0100

    initial code

---
+++ mobileHeader.js
@@ -0,0 +1,66 @@
+(function initialize($) {
+
+  $.fn.mobileHeader = function mobileHeader(
+    {delay, pointer} = {}) {
+    delay = delay || 50;
+    const articles = $('article');
+    const $this = $(this);
+    const $headerTitle = $this.children('.headerTitle');
+    const $title = $('title');
+    let lastScrollTop = 0;
+    let startScrollTop = 0;
+    let isLastDown = false;
+
+    $(window).on('scroll.mobileHeader', ()=> {
+      const scrollTop = $(window).scrollTop();
+      const offset = pointer || window.innerHeight / 3;
+      const isAnimated = $this.is(':animated');
+      const isDown = scrollTop > lastScrollTop;
+
+      if(isDown === !isLastDown)
+        startScrollTop = scrollTop;
+      else if(!isAnimated) {
+        const scrollDistance = scrollTop - startScrollTop;
+
+        if(isDown) {
+          if(scrollDistance > delay)
+            hide($this);
+        }
+        else {
+          if(scrollDistance < -delay)
+            show($this);
+        }
+      }
+      let distance = 0;
+
+      for(const article of articles) {
+        if((distance = scrollTop - article.offsetTop + offset) > 0) {
+          if(distance < article.offsetHeight) {
+            $headerTitle.html(article.getAttribute('articleTitle'));
+            $title.html(article.getAttribute('windowTitle'));
+            break;
+          }
+        }
+      }
+
+      isLastDown = isDown;
+      lastScrollTop = scrollTop;
+
+      function show(element) {
+        element.css('display', 'block');
+        element.clearQueue().animate({opacity: 1});
+      }
+
+      function hide(element) {
+        element.clearQueue().animate({opacity: 0},
+          {complete: ()=>
+            element.css('display', 'none')
+        });
+        element.children('nav.slide-nav')
+          .toggleClass('expanded', false);
+      }
+    });
+  };
+})(jQuery);
+
+export {};
